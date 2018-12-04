a832571237b6b9263fd7ba8d878f6dc6461d49ed
Roger Allen <rallen@nvidia.com>
2018-12-03 20:19:50 -0800

    day3 done

--- advent_of_code_2018/Day03.java
+++ advent_of_code_2018/Day03.java
@@ -1,5 +1,95 @@
 package advent_of_code_2018;
 
-public class Day03 {
+import java.util.stream.Stream;
+import java.util.regex.Pattern;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
 
+
+public class Day03 {
+	public static final int DIM = 1000;
+	public static int[] cloth = new int[DIM*DIM];
+	public static Set<Integer> claims = new HashSet<Integer>();
+	
+	public static class ClothRect {
+		public static int claim, x, y, w, h;
+		public static void parse(String s) {
+			// #1 @ 1,3: 4x4
+			// #(\d+) @ (\d+),(\d+): (\d+)x(\d+)
+			Pattern pattern = Pattern.compile("#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)");
+	        Matcher matcher = pattern.matcher(s);
+
+	        if(matcher.find()) {
+	            claim = Integer.parseInt(matcher.group(1));
+	            claims.add(claim);
+	            x = Integer.parseInt(matcher.group(2));
+	            y = Integer.parseInt(matcher.group(3));
+	            w = Integer.parseInt(matcher.group(4));
+	            h = Integer.parseInt(matcher.group(5));
+	        } else {
+	        	System.err.println("NO MATCH: "+s);
+	        }
+		}
+		public static void rasterize() {
+			for(int j = y; j < y + h; j++) {
+				for(int i = x; i < x + w; i++) {
+					cloth[j*DIM + i] += 1;
+				}
+			}
+		}
+		public static void rasterizeOverlap() {
+			for(int j = y; j < y + h; j++) {
+				for(int i = x; i < x + w; i++) {
+					if(cloth[j*DIM + i] != 0) {
+						// overlap case
+						claims.remove(cloth[j*DIM + i]);
+						claims.remove(claim);
+					}
+					cloth[j*DIM + i] = claim;
+				}
+			}
+		}
+	}
+	
+	public static void clear() {
+		for(int y = 0; y < DIM; y++) {
+			for(int x = 0; x < DIM; x++) {
+				cloth[y*DIM + x] = 0;
+			}
+		}
+	}
+
+	public static int solve1(Stream<String> inputStream) {
+		String [] inputs = inputStream.toArray(String[]::new);
+		clear();
+		for(String s: inputs) {
+			ClothRect.parse(s);
+			ClothRect.rasterize();
+		}
+		
+		int overlap = 0;
+		for(int y = 0; y < DIM; y++) {
+			for(int x = 0; x < DIM; x++) {
+				if(cloth[y*DIM + x] > 1) {
+					overlap += 1;
+				}
+			}
+		}
+		return overlap;
+	}
+
+	public static int solve2(Stream<String> inputStream) {
+		String [] inputs = inputStream.toArray(String[]::new);
+		clear();
+		claims.clear();
+		for(String s: inputs) {
+			ClothRect.parse(s);
+			ClothRect.rasterizeOverlap();
+		}
+		if(claims.size() != 1) {
+			System.err.println("More than one claim remains!"+claims.toString());
+		}
+		return claims.iterator().next();
+	}
 }
--- advent_of_code_2018/Test03.java
+++ advent_of_code_2018/Test03.java
@@ -2,13 +2,39 @@ package advent_of_code_2018;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 
 class Test03 {
 
+	String INPUT_FILE = "input/day03.txt";
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test1() {
+		assertEquals(Day03.solve1(Stream.of(
+				"#1 @ 1,3: 4x4",
+				"#2 @ 3,1: 4x4",
+				"#3 @ 5,5: 2x2")), 4);
+	}
+	
+	@Test
+	void testPart1() throws IOException {
+		assertEquals(Day03.solve1(Utils.fileToStringStream(INPUT_FILE)), 110827);
+	}
+
+	@Test
+	void test2() {
+		assertEquals(Day03.solve2(Stream.of(
+				"#1 @ 1,3: 4x4",
+				"#2 @ 3,1: 4x4",
+				"#3 @ 5,5: 2x2")), 3);
+	}
+	
+	@Test
+	void testPart2() throws IOException {
+		assertEquals(Day03.solve2(Utils.fileToStringStream(INPUT_FILE)), 116);
 	}
 
 }
